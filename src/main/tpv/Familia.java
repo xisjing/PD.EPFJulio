a27a179f8ca3f0696eac36966196c04c312b57f2
sijing <xisijing@gmail.com>
2016-06-28 17:58:09 +0200

    inplementar la clase Famili

--- src/main/tpv/Familia.java
+++ src/main/tpv/Familia.java
@@ -1,5 +1,20 @@
 package main.tpv;
 
-public abstract class Familia{
+public class Familia extends Producto{
+	
+	private String familia;
+
+	public Familia(String name, float precio,String familia) {
+		super(name, precio);
+		this.setFamilia(familia);
+	}
+
+	public String getFamilia() {
+		return familia;
+	}
+
+	public void setFamilia(String familia) {
+		this.familia = familia;
+	}
 	
 }
